eeec670a7e41275fa9ef276a1ea9a4592e1a6e83
KobeSagami <44218804+KobeSagami@users.noreply.github.com>
2019-02-05 14:43:38 -0700

    Added group.c

---
+++ group.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Group10
+//
+//  Created by Kobe Sagami on 2/5/19.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    member1();
+    member2();
+    member3();
+    member4();
+    
+    return 0;
+}
